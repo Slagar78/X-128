17bc5cc03eb77dd30e82866fc3ec326c943b888b
Slagar78 <rolemon95@gmail.com>
2026-04-13 11:23:15 +0300

    x-128

--- nuklear_ui/font.h
+++
@@ -1,6 +0,0 @@
-#ifndef FONT_H_
-#define FONT_H_
-
-uint8_t *default_font(uint32_t *size_out);
-
-#endif //FONT_H_
--- svp_util.c
+++
@@ -1,6 +0,0 @@
-
-void svp_prog_read_16(svp_context *context)
-{
-	uint16_t address = context->scratch1 >> 1;
-	context->scratch1 = context->rom[address];
-}
